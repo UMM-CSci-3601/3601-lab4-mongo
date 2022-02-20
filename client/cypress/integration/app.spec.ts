c21b7994a657e1495083f49aae1dbd18b4f4e49f
Nic McPhee <mcphee@morris.umn.edu>
2022-02-20 11:05:41 -0600

    Replace all "Iteration Template" titles with "Lab 4"

--- client/cypress/integration/app.spec.ts
+++ client/cypress/integration/app.spec.ts
@@ -6,7 +6,7 @@ describe('App', () => {
   beforeEach(() => page.navigateTo());
 
   it('Should have the correct title', () => {
-    page.getAppTitle().should('contain', 'CSCI 3601 Iteration Template');
+    page.getAppTitle().should('contain', 'CSci 3601 Lab 4');
   });
 
   it('The sidenav should open, navigate to "Users" and back to "Home"', () => {
