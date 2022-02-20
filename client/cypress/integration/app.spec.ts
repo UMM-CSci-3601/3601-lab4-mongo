e450b51830290c3c362b55b45db3b5ebab3a34c1
Nic McPhee <mcphee@morris.umn.edu>
2022-02-20 11:09:44 -0600

    Replace the sidenav Cypress description with the one from Lab 3
    Lab 3's Cypress test for the sidenav in `app.spec.ts` makes a lot more sense than the test in either Lab 4 or the iteration template, so I'm copying it over here.

--- client/cypress/integration/app.spec.ts
+++ client/cypress/integration/app.spec.ts
@@ -9,25 +9,39 @@ describe('App', () => {
     page.getAppTitle().should('contain', 'CSci 3601 Lab 4');
   });
 
-  it('The sidenav should open, navigate to "Users" and back to "Home"', () => {
-    // Before clicking on the button, the sidenav should be hidden
-    page.getSidenav()
-      .should('be.hidden');
+  describe('Sidenav', () => {
+    it('Should be invisible by default', () => {
+      // Before clicking on the button, the sidenav should be hidden
+      page.getSidenav()
+        .should('be.hidden')
+        .and('not.be.visible');
+    });
+
+    it('Should be openable by clicking the sidenav button', () => {
+      page.getSidenavButton().click();
+
+      page.getSidenav()
+        .should('not.be.hidden')
+        .and('be.visible');
+    });
+
+    it('Should have a working navigation to "Users"', () => {
+      page.getSidenavButton().click();
+      page.getSidenav();
+      // When we click the "Users" option in the side navbar…
+      page.getNavLink('Users').click();
+      // …then the URL of the current page should change to "…/users".
+      cy.url().should('match', /.*\/users$/);
+    });
 
 
-    page.getSidenavButton().click()
-      .should('be.visible');
-
-    page.getNavLink('Users').click();
-    cy.url().should('match', /\/users$/);
-    page.getSidenav()
-      .should('be.hidden');
-
-    page.getSidenavButton().click();
-    page.getNavLink('Home').click();
-    cy.url().should('match', /^https?:\/\/[^\/]+\/?$/);
-    page.getSidenav()
-      .should('be.hidden');
+    it('Should have a working navigation to "Home"', () => {
+      page.getSidenavButton().click();
+      // When we click the "Home" option in the side navbar…
+      page.getNavLink('Home').click();
+      // …then the URL of the current page should change to "…/".
+      cy.url().should('match', /.*\/$/);
+    });
   });
 
 });
