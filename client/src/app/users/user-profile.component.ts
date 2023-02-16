130e41bea73cf03fe6dbf981419482fa336e98b6
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-15 19:10:36 -0600

    Update `UserProfileComponent` in a recommended way
    Much of the work done in this commit is modeled after a suggestion in this StackOverflow answer: https://stackoverflow.com/questions/38008334/angular-rxjs-when-should-i-unsubscribe-from-subscription#:~:text=In%20most%20cases%20we%20will,error()%20messages%20are%20published
    
    This set of changes not only makes sure that subscription is handled in a way that completes any observables, but also means we don't need to (somewhat artificially) trigger any updates in the component that allow us to poke around in the paramMap and get new user information.
    
    We also add the MatSnackBar for giving information to the user of the webpage if loading the user doesn't work instead of just putting that information in the console.
    
    Co-authored-by: Nic McPhee <mcphee@morris.umn.edu>

--- client/src/app/users/user-profile.component.ts
+++ client/src/app/users/user-profile.component.ts
@@ -1,42 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from './user';
 import { UserService } from './user.service';
-import { first, map, switchMap } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { map, switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit {
-
+export class UserProfileComponent implements OnInit, OnDestroy {
   user: User;
 
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  // This `Subject` will only ever emit one (empty) value when
+  // `ngOnDestroy()` is called, i.e., when this component is
+  // destroyed. That can be used ot tell any subscriptions to
+  // terminate, allowing the system to free up their resources (like memory).
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
-    // The map and switchMap are each steps in the pipeline...
-    // the map step pays attention to ParamMap
-    // the result from the map step is the id string
-    // that gets used by the switchMap, which was expecting a string...
-    // it uses that string to get an Observable<User>
-    //
+    // The `map`, `switchMap`, and `takeUntil` are all RXJS operators, and
+    // each represents a step in the pipeline built using the RXJS `pipe`
+    // operator.
+    // The map step takes the `ParamMap` from the `ActivatedRoute`, which
+    // is typically the URL in the browser bar.
+    // The result from the map step is the `id` string for the requested
+    // `User`.
+    // That ID string gets passed (by `pipe`) to `switchMap`, which transforms
+    // it into an Observable<User>, i.e., all the (zero or one) `User`s
+    // that have that ID.
+    // The `takeUntil` operator allows this pipeline to continue to emit values
+    // until `this.ngUnsubscribe` emits a value, saying to shut the pipeline
+    // down and clean up any associated resources (like memory).
     this.route.paramMap.pipe(
-      // Hey! The paramMap changed... map the paramMap into the id
+      // Map the paramMap into the id
       map((paramMap: ParamMap) => paramMap.get('id')),
-      // maps the Observable<string> (i.e., the id) into the Observable<User>
-      // an observable... for each id that comes in, process and return an observable of all the results from that thing that cme in
+      // Maps the `id` string into the Observable<User>,
+      // which will emit zero or one values depending on whether there is a
+      // `User` with that ID.
       switchMap((id: string) => this.userService.getUserById(id)),
-      // We only ever want the first value from the paramMap `Observable`;
-      // limiting what we return here to that first value ensures that this
-      // pipeline terminates and the subscription is garbage collected.
-      first()
+      // Allow the pipeline to continue to emit values until `this.ngUnsubscribe`
+      // returns a value, which only happens when this component is destroyed.
+      // At that point we shut down the pipeline, allowed any
+      // associated resources (like memory) are cleaned up.
+      takeUntil(this.ngUnsubscribe)
     ).subscribe({
       next: user => this.user = user,
-      // This is terrible error handling; we should tell the user something.
-      error: err => console.log('Error getting a user for user profile' + err),
-      complete: () => console.log('We got a new user, and we are done!'),
-    }); // when something happens, update our user field
+      error: _err => {
+        this.snackBar.open('Problem loading the user – try again', 'OK', {
+          duration: 5000,
+        });
+      }
+      /*
+       * You can uncomment the line that starts with `complete` below to use that console message
+       * as a way of verifying that this subscription is completing.
+       * We removed it since we were not doing anything interesting on completion
+       * and didn't want to clutter the console log
+       */
+      // complete: () => console.log('We got a new user, and we are done!'),
+    });
+  }
+
+  ngOnDestroy() {
+    // When the component is destroyed, we'll emit an empty
+    // value as a way of saying that any active subscriptions should
+    // shut themselves down so the system can free up any associated
+    // resources, like memory.
+    this.ngUnsubscribe.next();
+    // Calling `complete()` says that this `Subject` is done and will
+    // never send any further values.
+    this.ngUnsubscribe.complete();
   }
 }
