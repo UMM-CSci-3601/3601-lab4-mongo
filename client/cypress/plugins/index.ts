ebaec46e28ae27ecd950b0802d0077a032f6c672
K.K. Lamberty <lamberty@morris.umn.edu>
2023-02-24 22:51:44 -0600

    Make minor changes to match iteration template

--- client/cypress/plugins/index.ts
+++ client/cypress/plugins/index.ts
@@ -18,7 +18,7 @@ const mongoDb = process.env.MONGO_DB || 'dev';
 const mongoUri = `mongodb://${mongoHost}/${mongoDb}`;
 const dbSeedDir = '../database/seed';
 
-const pluginConfig: Cypress.PluginConfig = (on, config) => {
+const pluginConfig: Cypress.PluginConfig = (on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('task', {
